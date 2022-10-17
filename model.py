ab03fed9f380230713b20a6b3dc81f860f7e5e3c
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-10-17 22:08:55 +0100

    Chapter 2: Build a Simple CRUD App

--- model.py
+++ model.py
@@ -12,3 +12,14 @@ class Todo(BaseModel):
                 "item": "Example schema!"
             }
         }
+
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
