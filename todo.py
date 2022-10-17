29ee0e52a234cee27f5dacab20f9e2c2c0d45219
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-10-17 16:54:57 +0100

    Chapter 2: Path and Query Parameters
    Add path and query parameters to the route handlers

--- todo.py
+++ todo.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Path
 from model import Todo
 
 todo_router = APIRouter()
@@ -16,3 +15,14 @@ async def add_todo(todo: Todo) -> dict:
 @todo_router.get("/todo")
 async def retrieve_todos() -> dict:
     return {"todos": todo_list}
+
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {"todo": todo}
+
+    return {
+        "message": "Todo with supplied ID doesn't exist."
+    }
