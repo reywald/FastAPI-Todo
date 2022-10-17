7577249300fd53f267ddd180165a463648a8658a
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-10-17 21:09:23 +0100

    Chapter 2: Request Body
    Declare Request body example data

--- model.py
+++ model.py
@@ -1,5 +1,14 @@
 from pydantic import BaseModel
 
+
 class Todo(BaseModel):
-  id: int
-  item: str
+    id: int
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "item": "Example schema!"
+            }
+        }
