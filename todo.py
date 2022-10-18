c8d4f0138f206dfef8e17309776af38a52a92612
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-10-18 10:27:54 +0100

    Chapter 4: Templating in FastAPI

--- todo.py
+++ todo.py
@@ -1,27 +1,41 @@
-from fastapi import APIRouter, Path, HTTPException, status
+from urllib import request
+from fastapi import APIRouter, Path, HTTPException, status, Depends, Request
+from fastapi.templating import Jinja2Templates
 from model import Todo, TodoItem, TodoItems
 
 todo_router = APIRouter()
 
 todo_list = []
 
+templates = Jinja2Templates(directory="templates/")
+
 
 @todo_router.post("/todo", status_code=status.HTTP_201_CREATED)
-async def add_todo(todo: Todo) -> dict:
+async def add_todo(request: Request, todo: Todo = Depends(Todo.as_form)):
+    todo.id = len(todo_list) + 1
     todo_list.append(todo)
-    return {"message": "Todo added successfully"}
+    return templates.TemplateResponse("todo.html", {
+        "request": request,
+        "todos": todo_list
+    })
 
 
 @todo_router.get("/todo", response_model=TodoItems)
-async def retrieve_todos() -> dict:
-    return {"todos": todo_list}
+async def retrieve_todos(request: Request):
+    return templates.TemplateResponse("todo.html", {
+        "request": request,
+        "todos": todo_list
+    })
 
 
 @todo_router.get("/todo/{todo_id}")
-async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
+async def get_single_todo(request: Request, todo_id: int = Path(..., title="The ID of the todo to retrieve.")):
     for todo in todo_list:
         if todo.id == todo_id:
-            return {"todo": todo}
+            return templates.TemplateResponse("todo.html", {
+                "request": request,
+                "todos": todo_list
+            })
 
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
@@ -30,7 +44,7 @@ async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to 
 
 
 @todo_router.put("/todo/{todo_id}")
-async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The ID of the todo to be updated")) -> dict:
+async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The ID of the todo to be updated")):
     for todo in todo_list:
         if todo.id == todo_id:
             todo.item = todo_data.item
